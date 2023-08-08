885922b9a2f584201cc8b94c9ce4ec226867355a
aliia gismatullina <angismatullina@gmail.com>
2023-08-08 15:38:03 +1000

    update added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,29 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    # for the form!
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      # give the form back again --> new.html.erb
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # read all
   get '/tasks', to: 'tasks#index', as: :tasks
+  # create 1) form 2) creation
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create' # no prefix here
+  # read one
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  # update 1) form 2) updation
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
 end
